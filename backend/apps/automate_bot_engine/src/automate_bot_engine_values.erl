cff2d662fe9dd6a495336b7cd63e8bda94880f36
kenkeiras <kenkeiras@codigoparallevar.com>
2018-06-28 21:28:09 +0200

    Keep integer variables as integer if possible.

--- backend/apps/automate_bot_engine/src/automate_bot_engine_values.erl
+++ backend/apps/automate_bot_engine/src/automate_bot_engine_values.erl
@@ -10,6 +10,8 @@
 -spec add(binary(), binary()) -> binary().
 add(Previous, Change) when is_binary(Previous) and is_binary(Change) ->
     case combined_type(Previous, Change) of
+        {integer, PreviousInt, ChangeInt} ->
+            erlang:integer_to_binary(PreviousInt + ChangeInt);
         {float, PreviousF, ChangeF} ->
             erlang:float_to_binary(PreviousF + ChangeF);
         {string, PreviousS, ChangeS} ->
@@ -17,11 +19,24 @@ add(Previous, Change) when is_binary(Previous) and is_binary(Change) ->
     end.
 
 combined_type(V1, V2) when is_binary(V1) and is_binary(V2) ->
-    case [to_float(V1), to_float(V2)] of
-        [{ok, F1}, {ok, F2}] ->
-            {float, F1, F2};
+    case [to_int(V1), to_int(V2)] of
+        [{ok, Int1}, {ok, Int2}] ->
+            {integer, Int1, Int2};
         _ ->
-            {string, V1, V2}
+            case [to_float(V1), to_float(V2)] of
+                [{ok, F1}, {ok, F2}] ->
+                    {float, F1, F2};
+                _ ->
+                    {string, V1, V2}
+            end
+    end.
+
+to_int(Value) when is_binary(Value) ->
+    case string:to_integer(Value) of
+        {Int, <<"">>} when is_integer(Int) ->
+            {ok, Int};
+        X = {error, _Reason} ->
+            X
     end.
 
 to_float(Value) when is_binary(Value) ->
