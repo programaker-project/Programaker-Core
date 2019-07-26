078093062dfdee06be4f29ba5b7e32e5f546d612
kenkeiras <me@codigoparallevar.com>
2019-07-26 23:48:05 +0200

    Add backend image optimization.

---
+++ utils/ci-preparations/optimize-backend-image.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eu
+
+SOURCE_IMAGE="$1"
+DESTINATION_IMAGE="$2"
+TMP_FILE="${3:-OPTIMIZED_IMAGE.dockerfile}"
+[ ! -f "${TMP_FILE}" ] # Check that TMP_FILE does not exist
+
+TEMPLATE="FROM alpine as final
+
+RUN apk add ncurses libstdc++ erlang
+
+COPY --from=${SOURCE_IMAGE} /app/_build/default/rel/automate/ /app/release/
+ADD ./scripts/ /app/scripts
+
+# API server port
+EXPOSE 8888
+
+# Launch directly the release
+CMD ['/app/scripts/container_init.sh']
+"
+
+echo "$TEMPLATE" > "${TMP_FILE}"
+docker build -t "${DESTINATION_IMAGE}" -f "${TMP_FILE}" .
+rm "${TMP_FILE}"
