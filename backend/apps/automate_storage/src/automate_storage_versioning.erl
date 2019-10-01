23ee62c50a45748efe012c61a797a456a6c66f3f
kenkeiras <me@codigoparallevar.com>
2019-10-01 23:43:32 +0200

    Add base logic for DB update.

--- backend/apps/automate_storage/src/automate_storage_versioning.erl
+++ backend/apps/automate_storage/src/automate_storage_versioning.erl
@@ -13,7 +13,9 @@
 -include("./databases.hrl").
 
 -spec apply_versioning(#database_version_progression{}, [node()]) -> ok | {error, atom}.
-apply_versioning(#database_version_progression{base=Base, updates=_Updates}, Nodes) ->
+apply_versioning(#database_version_progression{base=Base, updates=Updates}, Nodes) ->
+    ok = check_updates_integrity(Updates),
+
     DBNames = lists:map(fun(DBData = #database_version_data{ database_name=DBName }) ->
                                 create_database(DBData, Nodes),
                                 DBName
@@ -21,9 +23,9 @@ apply_versioning(#database_version_progression{base=Base, updates=_Updates}, Nod
 
     ok = mnesia:wait_for_tables(DBNames, automate_configuration:get_table_wait_time()),
 
-    %% @WIP: Apply updates
+    CurrentDatabaseVersion = get_database_version(),
 
-    ok.
+    ok = apply_updates_after_version(CurrentDatabaseVersion, Updates).
 
 
 -spec create_database(#database_version_data{}, [node()]) -> ok.
@@ -41,4 +43,46 @@ create_database(#database_version_data{ database_name=DBName
             ok;
         { aborted, { already_exists, _ }} ->
             ok
+    end.
+
+
+get_database_version() ->
+    %% @WIP: Write code to extract database version.
+    0.
+
+set_database_version(VersionNumber) ->
+    %% @WIP: Write code to set database version.
+    ok.
+
+
+apply_updates_after_version(_, []) ->
+    ok;
+
+apply_updates_after_version(OldDatabaseVersion, [#database_version_transformation{ id=Id }
+                                                 | T])  when Id =< OldDatabaseVersion ->
+    apply_updates_after_version(OldDatabaseVersion, T);
+
+
+apply_updates_after_version(OldDatabaseVersion, [#database_version_transformation{ id=Id
+                                                                                 , apply=Fun
+                                                                                 }
+                                                | T])  when Id > OldDatabaseVersion ->
+    Fun(),
+    set_database_version(Id),
+    apply_updates_after_version(Id, T).
+
+
+check_updates_integrity(Updates) ->
+    check_updates_integrity(0, Updates).
+
+check_updates_integrity(_, []) ->
+    ok;
+check_updates_integrity(MinVersionLessOne, [ Update=#database_version_transformation{ id=Id
+                                                                                    , apply=Fun
+                                                                                    }
+                                             | T ]) when is_function(Fun) ->
+    if Id < MinVersionLessOne ->
+            {error, version_number_not_increasing, Update};
+       true ->
+            check_updates_integrity(Id, T)
     end.
