0082ab40452e0f13ce7e0c3f435f4366dfc53d5f
kenkeiras <me@codigoparallevar.com>
2019-11-18 23:40:30 +0100

    Remove tag autocompletion.
    The reason for this is that the auto-completion only worked with already
    present tags. As there's no reason for tags to be multiple times, they
    should be deduplicated.
    
    Also, for this reason, there's no point in facilitating writing them
    twice. For this reason, and because that could be confusing, the
    autocompletion functionality is removed for now.
    
    Hopefully it can come in handy later, at which point it's possible to
    revert this single commit.

--- frontend/src/app/program_tags/SetProgramTagsDialogComponent.ts
+++ frontend/src/app/program_tags/SetProgramTagsDialogComponent.ts
@@ -2,8 +2,6 @@ import { Component, ElementRef, ViewChild, Input, Inject } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 import { FormControl } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -36,12 +34,9 @@ export class SetProgramTagsDialogComponent {
     addOnBlur = true;
     separatorKeysCodes: number[] = [ENTER, COMMA];
     tagCtrl = new FormControl();
-    filteredTags: Observable<string[]>;
-    allTags: string[] = [];
     tags: string[] = [];
 
     @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
-    @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
     constructor(public dialogRef: MatDialogRef<SetProgramTagsDialogComponent>,
                 private programService: ProgramService,
@@ -57,14 +52,9 @@ export class SetProgramTagsDialogComponent {
         this.program_name = this.program.name;
 
         this.programService = programService;
-        this.filteredTags = this.tagCtrl.valueChanges.pipe(
-            startWith(null),
-            map((tag: string | null) => tag ? this._filter(tag) : this.allTags.slice()));
 
         data.tags = this.tags;
         this.programService.getProgramTags(this.programUserId, this.program.id).then((tags) => {
-            this.allTags = tags;
-
             // Bind data.tags again now that this.tags has changed
             data.tags = this.tags = tags;
         });
@@ -75,25 +65,20 @@ export class SetProgramTagsDialogComponent {
     }
 
     add(event: MatChipInputEvent): void {
-        // Add tag only when MatAutocomplete is not open
-        // To make sure this does not conflict with OptionSelected Event
-        if (!this.matAutocomplete.isOpen) {
-            const input = event.input;
-            const value = event.value;
-
-            // Add our tag
-            if ((value || '').trim()) {
-                this.tags.push(value.trim());
-                this.deduplicateTags();
-            }
-
-            // Reset the input value
-            if (input) {
-                input.value = '';
-            }
-
-            this.tagCtrl.setValue(null);
+        const input = event.input;
+        const value = event.value;
+        // Add our tag
+        if ((value || '').trim()) {
+            this.tags.push(value.trim());
+            this.deduplicateTags();
         }
+
+        // Reset the input value
+        if (input) {
+            input.value = '';
+        }
+
+        this.tagCtrl.setValue(null);
     }
 
     // Remove duplicated tags
@@ -120,16 +105,4 @@ export class SetProgramTagsDialogComponent {
         }
     }
 
-    selected(event: MatAutocompleteSelectedEvent): void {
-        this.tags.push(event.option.viewValue);
-        this.tagInput.nativeElement.value = '';
-        this.tagCtrl.setValue(null);
-        this.deduplicateTags();
-    }
-
-    private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
-
-        return this.allTags.filter(tag => tag.toLowerCase().indexOf(filterValue) === 0);
-    }
 }
