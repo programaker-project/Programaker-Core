a9b909df828fd877bb008a75ab55df406b166ad6
Sergio Martínez Portela <sergio@codigoparallevar.com>
2020-03-18 13:06:35 +0100

    Add email to admin panel, reword messages column.
    - Also make username length notes warnings, instead of errors.

--- frontend/src/app/settings/admin-settings.component.ts
+++ frontend/src/app/settings/admin-settings.component.ts
@@ -88,7 +88,7 @@ export class AdminSettingsComponent {
 
         if ((user.username.length < 4) || (user.username.length > 50)){
             notes.push({
-                icon: "error",
+                icon: "warning", // This is not allowed, but can be handled
                 text: "User name should have at more than 3 and at most 50 characters",
             });
         }
