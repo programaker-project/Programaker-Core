9c812f4a45eff29ca592fa705a14d119e66b947e
Sergio Martínez Portela <sergio@codigoparallevar.com>
2020-04-27 16:59:18 +0200

    Parallelize example graph generation.

--- frontend/src/app/tests/logic/flow-graph-analysis/_gen_graphs.utils.ts
+++ frontend/src/app/tests/logic/flow-graph-analysis/_gen_graphs.utils.ts
@@ -1,33 +1,42 @@
 import { spawn } from 'child_process';
 import * as fs from 'fs';
 import { convert_to_graphviz } from '../scaffolding/utils';
+import * as util from 'util';
 
-const files = fs.readdirSync(__dirname);
-files.forEach((file: string) => {
-    if (file.match(/\d+.*.spec.ts$/)) {
-        process.stdout.write(`Loading ${file} ... `)
+export function run(): Promise<any[]> {
+    const files = fs.readdirSync(__dirname);
 
-        const mod = require('./' + file.substr(0, file.length - 3)); // Remove '.ts'
-        if (mod.gen_flow) {
-            try {
-                fs.writeFileSync(`${__dirname}/${file}.dot`, convert_to_graphviz(mod.gen_flow()));
-                spawn("dot", ["-Tpng", `${__dirname}/${file}.dot`, '-o', `${__dirname}/${file}.png`]);
+    const promises = files
+        .filter((file: string) => file.match(/\d+.*.spec.ts$/))
+        .map((file: string) => {
+            return new Promise(async (resolve, reject) => {
+                const mod_name = file.substr(0, file.length - 3);
+                const mod = require('./' + mod_name); // Remove '.ts'
+                if (mod.gen_flow) {
+                    try {
+                        await util.promisify(fs.writeFile)(`${__dirname}/${file}.dot`, convert_to_graphviz(mod.gen_flow()));
+                        spawn("dot", ["-Tpng", `${__dirname}/${file}.dot`, '-o', `${__dirname}/${file}.png`]);
 
-                if (mod.process_flow) {
-                    fs.writeFileSync(`${__dirname}/${file}.processed.dot`, convert_to_graphviz(mod.process_flow(mod.gen_flow())));
-                    spawn("dot", ["-Tpng", `${__dirname}/${file}.processed.dot`, '-o', `${__dirname}/${file}.processed.png`]);
+                        if (mod.process_flow) {
+                            await util.promisify(fs.writeFile)(`${__dirname}/${file}.processed.dot`,
+                                                               convert_to_graphviz(mod.process_flow(mod.gen_flow())));
+                            spawn("dot", ["-Tpng", `${__dirname}/${file}.processed.dot`, '-o', `${__dirname}/${file}.processed.png`]);
+                        }
 
-                    process.stdout.write('+proc ')
+                        process.stdout.write(`${file} OK\n`)
+                        resolve(file);
+                    }
+                    catch (err) {
+                        process.stdout.write(file + ': ' + err.toString() + '\n');
+                        reject(err);
+                    }
                 }
+                else {
+                    process.stdout.write('No flow found\n');
+                    resolve(null);
+                }
+            });
+        });
 
-                process.stdout.write('OK\n');
-            }
-            catch (err) {
-                process.stdout.write(err.toString() + '\n');
-            }
-        }
-        else {
-            process.stdout.write('No flow found\n');
-        }
-    }
-});
+    return Promise.all(promises) as Promise<any[]>;
+}
