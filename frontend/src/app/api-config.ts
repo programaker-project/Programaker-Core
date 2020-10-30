0cd98155d3f53b1249c3faa72d72d1925a923af6
Sergio Martínez Portela <sergio@codigoparallevar.com>
2020-10-30 23:40:16 +0100

    WIP: Introduce base structure change. [skip ci]

--- frontend/src/app/api-config.ts
+++
@@ -1,6 +0,0 @@
-import { environment } from '../environments/environment';
-
-const ApiHost = environment.ApiHost;
-const ApiRoot = ApiHost + '/api/v0';
-
-export { ApiHost, ApiRoot };
---
+++ frontend/src/app/environment.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { environment } from 'environments/environment';
+import { isPlatformServer } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvironmentService {
+    environment: {
+        contact_mail?: string;
+        production: boolean;
+        ApiHost: string;
+        SSRApiHost?: string;
+    };
+
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: Object
+    ) {
+        this.environment = environment;
+    }
+
+    getApiHost(): string {
+        if (this.environment.SSRApiHost && isPlatformServer(this.platformId)) {
+            return this.environment.SSRApiHost;
+        }
+
+        return this.environment.ApiHost;
+    }
+
+    getApiRoot(): string {
+        return this.getApiHost() + '/api/v0';
+    }
+}
