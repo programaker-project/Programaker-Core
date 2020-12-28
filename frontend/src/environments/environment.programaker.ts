d0efa5b3cfa56335c99e2cd46715c13f98f7aca3
Sergio Martínez Portela <sergio@codigoparallevar.com>
2020-12-28 17:15:52 +0100

    Add secondary About-page rendering url for SSR.

--- frontend/src/environments/environment.programaker.ts
+++ frontend/src/environments/environment.programaker.ts
@@ -4,4 +4,5 @@ export const environment = {
     ApiHost: '',
     SSRApiHost: 'http://plaza-backend:80',
     aboutPageRender: 'https://programaker.com/api/v0/programs/by-id/5fad1d5e-9f69-4f7f-93db-cbb6816d8b22/render/',
+    SSRAboutPageRender: 'http://plaza-backend:80/api/v0/programs/by-id/5fad1d5e-9f69-4f7f-93db-cbb6816d8b22/render/',
 };
