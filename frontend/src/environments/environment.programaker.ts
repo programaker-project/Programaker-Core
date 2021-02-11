86fd5bb00b90d5826fc152d525e6acf84b9c4e67
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-02-11 20:03:51 +0100

    Set YjsSync server on PrograMaker environment.

--- frontend/src/environments/environment.programaker.ts
+++ frontend/src/environments/environment.programaker.ts
@@ -7,4 +7,5 @@ export const environment : EnvironmentDefinition = {
     SSRApiHost: 'http://plaza-backend:80',
     aboutPageRender: 'https://programaker.com/api/v0/programs/by-id/5fad1d5e-9f69-4f7f-93db-cbb6816d8b22/render/',
     SSRAboutPageRender: 'http://plaza-backend:80/api/v0/programs/by-id/5fad1d5e-9f69-4f7f-93db-cbb6816d8b22/render/',
+    YjsWsSyncServer: 'wss://yjs-sync.programaker.com',
 };
