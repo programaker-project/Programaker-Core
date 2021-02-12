877dc517550d04b69bd8d70f703e2018b516b6e1
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-02-12 18:53:01 +0100

    Make PrograMaker-dev (front) config use the real sync server.

--- frontend/src/environments/environment.programaker-dev.ts
+++ frontend/src/environments/environment.programaker-dev.ts
@@ -4,6 +4,7 @@ export const environment : EnvironmentDefinition = {
     contact_mail: 'contact@programaker.com',
     production: true,
     ApiHost: 'https://programaker.com',
-    aboutPageRender: null,
+    aboutPageRender: 'https://programaker.com/api/v0/programs/by-id/5fad1d5e-9f69-4f7f-93db-cbb6816d8b22/render/',
     SSRAboutPageRender: null,
+    YjsWsSyncServer: 'wss://yjs-sync.programaker.com',
   };
