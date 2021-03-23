f6baaf406cf87209c9f5aa1f91991ed47376da25
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-03-23 20:20:05 +0100

    Add scopes query-string to AuthorizeNewTokenComponent test.

--- frontend/src/app/components/authorize-new-token/authorize-new-token.component.spec.ts
+++ frontend/src/app/components/authorize-new-token/authorize-new-token.component.spec.ts
@@ -5,13 +5,36 @@ import { CookiesService } from '@ngx-utils/cookies';
 import { BrowserCookiesModule, BrowserCookiesService } from '@ngx-utils/cookies/browser';
 import { AuthorizeNewTokenComponent } from './authorize-new-token.component';
 import { ToastrModule } from 'ngx-toastr';
+import { ActivatedRoute, ParamMap, Params, convertToParamMap } from '@angular/router';
+import { ReplaySubject } from 'rxjs';
 
+
+class ActivatedRouteStub implements Partial<ActivatedRoute> {
+    private subject = new ReplaySubject<ParamMap>();
+
+    paramMap = this.subject.asObservable();
+
+    constructor(initialParams?: Params) {
+        this.setParamMap(initialParams);
+    }
+
+    setParamMap(params?: Params) {
+        const paramMap = convertToParamMap(params);
+        this.subject.next(paramMap);
+    }
+}
 
 describe('AuthorizeNewTokenComponent', () => {
     let component: AuthorizeNewTokenComponent;
     let fixture: ComponentFixture<AuthorizeNewTokenComponent>;
 
     beforeEach(async(() => {
+
+
+        const routeStub = new ActivatedRouteStub({
+            scopes: 'list_connections_established list_custom_blocks call_any_bridge',
+        });
+
         TestBed.configureTestingModule({
             imports: [
                 BrowserCookiesModule.forRoot(),
@@ -23,6 +46,7 @@ describe('AuthorizeNewTokenComponent', () => {
                 AuthorizeNewTokenComponent,
             ],
             providers: [
+                { provide: ActivatedRoute, useValue: routeStub },
                 {
                     provide: CookiesService,
                     useClass: BrowserCookiesService,
