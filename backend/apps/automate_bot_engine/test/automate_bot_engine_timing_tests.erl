702cd6909ebd4bc96fa1e572c1bb9d6c2513651d
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-03-26 23:07:22 +0100

    Test recovering of tasks with skipped start times. [skip ci]

--- backend/apps/automate_bot_engine/test/automate_bot_engine_timing_tests.erl
+++ backend/apps/automate_bot_engine/test/automate_bot_engine_timing_tests.erl
@@ -54,14 +54,16 @@ stop({_NodeName}) ->
 
 
 tests(_SetupResult) ->
-    %% Operations
-    %% Lists
+
+%%%% Triggers
     [ {"[Bot engine][Timing] Wait for time signal", fun wait_for_simple_time_signal/0}
     , {"[Bot engine][Timing] Wait for time signal without Timezone change", fun wait_for_time_signal_on_no_timezone_change/0}
     , {"[Bot engine][Timing] Wait for time signal on Timezone change", fun wait_for_time_signal_on_timezone_change/0}
+%%%% Service discontinuity
+    , {"[Bot engine][Timing discontinuity] Scheduled tasks are executed even with interruptions", fun scheduled_handles_interruptions/0}
     ].
 
-%%%% Operations
+%%%% Triggers
 wait_for_simple_time_signal() ->
     {_Username, _ProgramName, ProgramId} = ?UTILS:create_anonymous_program(),
     Thread = #program_thread{ position = [1]
@@ -194,6 +196,66 @@ wait_for_time_signal_on_timezone_change() ->
     %% ?assertEqual(this_should_not_work_yet, false),
     ok.
 
+%%%% Service discontinuity
+scheduled_handles_interruptions() ->
+    %% 1. Start at a given time
+    %% 2. Wait for the program to start
+    %% 3. Jump over the expected time
+    %% 4. Expect the scheduler to be run
+
+    StartTime = {{2021, 03, 26}, {7, 00, 00}},
+    ScheduledTime = <<"07:30:00">>,
+    JumpTime = {{2021, 03, 26}, {8, 00, 00}},
+
+    ok = automate_testing:set_corrected_time(StartTime),
+
+    ProgramData = [ { ?WAIT_FOR_MONITOR_COMMAND,
+                      #{ ?MONITOR_ID => #{ ?FROM_SERVICE => ?TIME_SERVICE_UUID  }
+                       , ?MONITOR_EXPECTED_VALUE => #{ <<"type">> => <<"constant">>
+                                                     , <<"value">> => ScheduledTime
+                                                     }
+                       , <<"timezone">> => <<"UTC">>
+                       }
+                    }
+                  , { ?COMMAND_LOG_VALUE, [constant_val(<<"after">>)] }
+                  ],
+
+    {Username, ProgramName, ProgramId} = ?UTILS:create_anonymous_program(),
+    %% Launch program
+    ?assertMatch({ok, ProgramId},
+                 automate_storage:update_program(
+                   Username, ProgramName,
+                   #stored_program_content{ type=scratch_program
+                                          , parsed=#{ <<"blocks">> => [ ?UTILS:build_ast(ProgramData) ]
+                                                    , <<"variables">> => []
+                                                    }
+                                          , orig= <<"*test*">>
+                                          , pages=#{}
+                                          })),
+
+    ?assertMatch(ok, automate_bot_engine_launcher:update_program(ProgramId)),
+
+    %% Check that program id alive
+    ?assertMatch(ok, wait_for_program_alive(ProgramId, 10, 100)),
+
+    {ok, ProgramPid} = automate_storage:get_program_pid(ProgramId),
+    ?assert(is_process_alive(ProgramPid)),
+
+    %% Jump to final time
+    ok = automate_testing:set_corrected_time(JumpTime),
+
+    %% Wait ~2 seconds, should be enough for the time signal to arrive
+    timer:sleep(2000),
+    {ok, LogsAfter} = automate_bot_engine:get_user_generated_logs(ProgramId),
+    MsgsAfter = [ M || #user_generated_log_entry{event_message=M} <- LogsAfter ],
+
+    automate_testing:unset_corrected_time(),
+
+    io:fwrite("Logs after signal: ~p~n", [MsgsAfter]),
+    ?assertMatch([ <<"after">> ], MsgsAfter),
+    ok.
+
+
 %%====================================================================
 %% Util functions
 %%====================================================================
@@ -201,3 +263,15 @@ constant_val(Val) ->
     #{ ?TYPE => ?VARIABLE_CONSTANT
      , ?VALUE => Val
      }.
+
+wait_for_program_alive(Pid, 0, SleepTime) ->
+    {error, timeout};
+
+wait_for_program_alive(ProgramId, TestTimes, SleepTime) ->
+    case automate_storage:get_program_pid(ProgramId) of
+        {ok, _} ->
+            ok;
+        {error, not_running} ->
+            timer:sleep(SleepTime),
+            wait_for_program_alive(ProgramId, TestTimes - 1, SleepTime)
+    end.
