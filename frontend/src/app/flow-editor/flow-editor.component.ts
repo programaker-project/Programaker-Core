a3459420b5508f603ed01f8e2a8edd0e8740f626
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-04-08 11:01:29 +0200

    Fix font family on non SVG-text objects (like ForeignObjects).

--- frontend/src/app/flow-editor/flow-editor.component.ts
+++ frontend/src/app/flow-editor/flow-editor.component.ts
@@ -608,7 +608,7 @@ export class FlowEditorComponent implements OnInit, AfterViewInit {
                                   { responseType: 'text' as 'json' }
                                  ).toPromise() as any as Promise<string>)) +
             // Supplement flow editor CSS with global styles that affect it
-            'text {font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }\n' +
+            '* {font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; }\n' +
             '/* ]]> */<');
 
         canvas.insertBefore(styles, canvas.firstChild);
