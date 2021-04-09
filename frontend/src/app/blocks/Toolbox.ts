3c05c8f6f21985c1eb83202a10225313a0c32d29
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-04-09 14:16:47 +0200

    Fix showing `bridge connect/disconnect` blocks on public programs.

--- frontend/src/app/blocks/Toolbox.ts
+++ frontend/src/app/blocks/Toolbox.ts
@@ -553,43 +553,44 @@ export class Toolbox {
             full_connection_dropdown.push([conn.name || 'default', conn.connection_id]);
         }
 
-        if (bridge_dropdown.length > 0) {
-            Blockly.Blocks['trigger_on_bridge_connected'] = {
-                init: function () {
-                    this.jsonInit({
-                        'id': 'trigger_on_bridge_connected',
-                        'message0': 'When bridge %1 connects',
-                        'args0': [
-                            {
-                                'type': 'field_dropdown',
-                                'name': 'BRIDGE1',
-                                options: bridge_dropdown,
-                            },
-                        ],
-                        'category': Blockly.Categories.event,
-                        'extensions': ['colours_advanced_operation', 'shape_hat']
-                    });
-                }
-            };
-
-            Blockly.Blocks['trigger_on_bridge_disconnected'] = {
-                init: function () {
-                    this.jsonInit({
-                        'id': 'trigger_on_bridge_disconnected',
-                        'message0': 'When bridge %1 connection STOPS',
-                        'args0': [
-                            {
-                                'type': 'field_dropdown',
-                                'name': 'BRIDGE1',
-                                options: bridge_dropdown,
-                            },
-                        ],
-                        'category': Blockly.Categories.event,
-                        'extensions': ['colours_advanced_operation', 'shape_hat']
-                    });
-                }
-            };
+        if (bridge_dropdown.length == 0) {
+            bridge_dropdown.push(["Unkown", '__plaza_internal_unlisted'])
         }
+        Blockly.Blocks['trigger_on_bridge_connected'] = {
+            init: function () {
+                this.jsonInit({
+                    'id': 'trigger_on_bridge_connected',
+                    'message0': 'When bridge %1 connects',
+                    'args0': [
+                        {
+                            'type': 'field_dropdown',
+                            'name': 'BRIDGE1',
+                            options: bridge_dropdown,
+                        },
+                    ],
+                    'category': Blockly.Categories.event,
+                    'extensions': ['colours_advanced_operation', 'shape_hat']
+                });
+            }
+        };
+
+        Blockly.Blocks['trigger_on_bridge_disconnected'] = {
+            init: function () {
+                this.jsonInit({
+                    'id': 'trigger_on_bridge_disconnected',
+                    'message0': 'When bridge %1 connection STOPS',
+                    'args0': [
+                        {
+                            'type': 'field_dropdown',
+                            'name': 'BRIDGE1',
+                            options: bridge_dropdown,
+                        },
+                    ],
+                    'category': Blockly.Categories.event,
+                    'extensions': ['colours_advanced_operation', 'shape_hat']
+                });
+            }
+        };
 
         Blockly.Blocks['operator_select_connection'] = {
             init: function () {
