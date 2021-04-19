897e4b14bef2962926f1e8cad1054936d60c614a
Sergio Martínez Portela <sergio@codigoparallevar.com>
2021-04-19 18:15:17 +0200

    Change monitor listener from `erlang:monitor` to `erlang:link`.
    This fixes a memory leak-like behavior when the same process gets monitored
    again and again.

--- backend/apps/automate_channel_engine/src/automate_channel_engine_listener_monitor.erl
+++ backend/apps/automate_channel_engine/src/automate_channel_engine_listener_monitor.erl
@@ -19,6 +19,7 @@ start_link() ->
     case automate_coordination:run_task_not_parallel(
            fun() ->
                    yes = global:re_register_name(?MODULE, self()),
+                   process_flag(trap_exit, true),
                    loop()
            end, ?MODULE) of
         {started, Pid} ->
@@ -45,16 +46,23 @@ monitor_listener(Pid) ->
 loop() ->
     receive
         { monitor, Answer, Pid } ->
-            erlang:monitor(process, Pid),
-            Result = ok,
+            %% A link() is used here instead of a monitor to avoid a memory
+            %% leak-like behavior when the same Pid is monitored again and
+            %% again.
+            %%
+            %% Actually here the monitoring is not bidirectional (the listener
+            %% process doesn't care about what happens with this daemon) but as
+            %% errors here should be fairly unusual and adding de-duplication
+            %% logic to this process will make it more complex we will skip it for now.
+            erlang:link(Pid),
             case Answer of
                 _ when is_pid(Answer) ->
-                    Answer ! { ?MODULE, Result };
+                    Answer ! { ?MODULE, ok };
                 _ ->
                     ok
             end,
             loop();
-        { 'DOWN', _Ref, process, Pid, _Reason } ->
+        { 'EXIT', Pid, _Reason } ->
             ok = ?BACKEND:remove_listener(Pid),
             loop();
         stop ->
